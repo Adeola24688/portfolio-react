20dcbda831bdabec5e5727fc8737ca0fd4dce55b
Shefiu Abdullahi <abdullahishefiu99@gmail.com>
2026-05-14 13:51:03 +0100

    feat: add new portfolio projects and re-index existing entries in mywork_data.js

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -7,35 +7,49 @@ import project6_img from "../assets/project_6.svg";
 const mywork_data = [
   {
     w_no: 1,
+    w_name: "Freelancer Website",
+    w_img:
+      "https://s.wordpress.com/mshots/v1/https://freelancer-mocha-beta.vercel.app/?w=1200",
+    w_url: "https://freelancer-mocha-beta.vercel.app/",
+  },
+  {
+    w_no: 2,
+    w_name: "Portfolio Website",
+    w_img:
+      "https://s.wordpress.com/mshots/v1/https://abdullahi.infinityfree.me/?w=1200",
+    w_url: "https://abdullahi.infinityfree.me/",
+  },
+  {
+    w_no: 3,
     w_name: "Crypto Place",
     w_img: screenshot_crypto,
     w_url: "https://abdullahcryptoplace.vercel.app/",
   },
   {
-    w_no: 2,
+    w_no: 4,
     w_name: "YouTube Clone",
     w_img: screenshot_youtube,
     w_url: "https://abdullah-youtube-clone.vercel.app/",
   },
   {
-    w_no: 3,
+    w_no: 5,
     w_name: "Netflix Clone",
     w_img:
       "https://s.wordpress.com/mshots/v1/https://abdullah-netflix-clone.vercel.app/?w=1200",
     w_url: "https://abdullah-netflix-clone.vercel.app/",
   },
   {
-    w_no: 4,
+    w_no: 6,
     w_name: "Web design",
     w_img: project4_img,
   },
   {
-    w_no: 5,
+    w_no: 7,
     w_name: "Web design",
     w_img: project5_img,
   },
   {
-    w_no: 6,
+    w_no: 8,
     w_name: "Web design",
     w_img: project6_img,
   },
